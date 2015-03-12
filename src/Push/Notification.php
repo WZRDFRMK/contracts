d86955bad2555adb079d12e9945446d4dce915ec
Kévin DUBUC <kevindubuc62@gmail.com>
2015-03-12 16:27:38 +0100

    add push notification contract

---
+++ src/Push/Notification.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Wzrd\Contracts\Push;
+
+interface Notification
+{
+	/**
+	 * Set the notification's message.
+	 *
+	 * @param  string $message
+	 * @return self
+	 */
+	public function setMessage($message);
+
+	/**
+	 * Get notification's message.
+	 *
+	 * @return string
+	 */
+	public function getMessage();
+
+	/**
+	 * Set the notification's data.
+	 *
+	 * @param  array  $data
+	 * @return self
+	 */
+	public function setData(array $data);
+
+	/**
+	 * Get notification's data.
+	 *
+	 * @return array
+	 */
+	public function getData();
+
+	/**
+	 * Add a platform to the notification.
+	 *
+	 * @param  string  $platform
+     * @param  array  $platform
+	 * @param  array  $options Optional
+	 * @return self
+	 */
+	public function addDevices($platform, array $devices, array $options = array());
+
+	/**
+	 * Get devices tokens.
+	 *
+	 * @return array
+	 */
+	public function getDevices();
+
+	/**
+	 * Get targeted platforms
+	 *
+	 * @return array
+	 */
+	public function getTargetedPlatforms();
+}
