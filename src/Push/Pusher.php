c030f462466f043e4a120b6c34cef4470fb3b03b
Kévin DUBUC <kevindubuc62@gmail.com>
2015-03-12 17:26:10 +0100

    improve push contract

--- src/Push/Pusher.php
+++ src/Push/Pusher.php
@@ -4,10 +4,11 @@ namespace Wzrd\Contracts\Push;
 
 interface Pusher
 {
-    /**
-	 * Push message.
-	 *
-	 * @param  array  $data
-	 */
-	public function push(array $data);
+	/**
+     * Push message.
+     *
+     * @param  Wzrd\Contracts\Push\Notification  $notification
+     * @param  array  $options
+     */
+    public function push(Notification $notification, array $options = array());
 }
