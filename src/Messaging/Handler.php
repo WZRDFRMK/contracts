d7a95a92b9e8720bbb32e9139208ec0fd7a9f29f
Kévin DUBUC <kevindubuc62@gmail.com>
2016-02-17 17:26:17 +0100

    remove handle function in handler contract (move to frmk)

--- src/Messaging/Handler.php
+++ src/Messaging/Handler.php
@@ -4,12 +4,5 @@ namespace WZRD\Contracts\Messaging;
 
 interface Handler
 {
-    /**
-     * Handle the message.
-     *
-     * @param WZRD\Contracts\Messaging\Message $message
-     *
-     * @return string
-     */
-    public function handle(Message $message);
+    //
 }
