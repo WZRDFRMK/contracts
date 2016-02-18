8ac11ae470cde865476d563c2143227b4a5887a6
Kévin DUBUC <kevindubuc62@gmail.com>
2016-02-18 11:18:22 +0100

    add logic message access to stream contract

--- src/Messaging/Stream.php
+++ src/Messaging/Stream.php
@@ -6,5 +6,26 @@ use IteratorAggregate;
 
 interface Stream extends IteratorAggregate
 {
-    //
+    /**
+     * Push message into the stream.
+     *
+     * @param WZRD\Contracts\Messaging\Message $message
+     *
+     * @return self
+     */
+    public function push(Message $message);
+
+    /**
+     * Shift a message off the beginning of the stream.
+     *
+     * @return WZRD\Contracts\Messaging\Message
+     */
+    public function shift();
+
+    /**
+     * Flush stream.
+     *
+     * @return self
+     */
+    public function flush();
 }
