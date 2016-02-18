e01d0a891399bc6785379918fa2d2ae066b70d83
kdubuc <kevindubuc62@gmail.com>
2016-02-18 09:01:18 +0000

    Revert "remove handle function in handler contract (move to frmk)"
    This reverts commit d7a95a92b9e8720bbb32e9139208ec0fd7a9f29f.

--- src/Messaging/Handler.php
+++ src/Messaging/Handler.php
@@ -4,5 +4,12 @@ namespace WZRD\Contracts\Messaging;
 
 interface Handler
 {
-    //
+    /**
+     * Handle the message.
+     *
+     * @param WZRD\Contracts\Messaging\Message $message
+     *
+     * @return string
+     */
+    public function handle(Message $message);
 }
